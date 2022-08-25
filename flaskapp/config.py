03f18ec315be001ac612dc3a34f13681a3518b3e
621944283 <youssefchaabi848@gmail.com>
2022-08-25 13:26:54 +0200

    make it better

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -3,7 +3,9 @@ import pymysql
 class Config:
     SECRET_KEY= 'd71e327c04c11549fc8889cc652e781b54669d00927627ed5ddbb8a1baf44a2c'
     CKEDITOR_ENABLE_CODESNIPPET = True
-    SQLALCHEMY_DATABASE_URI = 'postgres://pdoiadhnevckkg:223bf9332939941fb3fb1fe974b883ae0ceb20499178774b1e5167fcbf35a889@ec2-44-205-112-253.compute-1.amazonaws.com:5432/de6adjfvm5puh4'
+    SQLALCHEMY_DATABASE_URI =  "sqlite:///ayoub.db"
+       #'mysql+pymysql://youssef:pythonicyoussef@localhost/ayoub_db'
+    #'postgres://pdoiadhnevckkg:223bf9332939941fb3fb1fe974b883ae0ceb20499178774b1e5167fcbf35a889@ec2-44-205-112-253.compute-1.amazonaws.com:5432/de6adjfvm5puh4'
     #'mysql+pymysql://youssef:pythonicyoussef@localhost/ayoub_db'
     #"sqlite:///ayoub.db"
 
